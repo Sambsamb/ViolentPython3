0f022d6ca7db4f282704216ec2c3cf3ae067c6a1
Samb <samb@townsware.com>
2022-11-03 10:44:42 -0400

    SB - update

---
+++ VP220-DNS/FlagVP220.6.py
@@ -0,0 +1,36 @@
+"""
+INF601 - Advanced Programming in Python
+Sam Boutros
+Prof. Zeller
+FHSU - Fall 2022
+11/3/2022
+https://samsclass.info/124/proj14/VP220.htm
+"""
+
+# In a Web browser, open this page:
+# https://samsclass.info/124/proj14/VP220-6.php
+# Monitor the DNS requests. Some of the requests contain the flag, but they are encoded
+
+from scapy.all import *
+import sys
+
+def findDNS(p):
+    if p.haslayer(DNS):
+        msg = p[DNS].summary()
+        if 'samsclass.info' in msg:
+            # Parse out the server DNS name
+            server_name = str(msg).split('"')[1].split('.')[0].split("'")[1]
+            # Convert binary to hex
+            bin2hex = (hex(int(server_name, 2)))
+            # Remove the 0x
+            my_str = bin2hex.replace('0x', '')
+            # Decode
+            decoded = bytes.fromhex(my_str).decode()
+            if '-' in decoded:
+                print(decoded)
+                sys.exit()
+
+print("Listening on DNS traffic for '-'")
+sniff(prn=findDNS)
+
+# -8bits-
